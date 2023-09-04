743b34ffad3d433b79eb41fff7accdab8fe567af
kazim110 <kazim.mohammadi110@gmail.com>
2023-09-05 03:23:37 +0430

    Initialize My Profile page: Set up route, view component, and navigation links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/myprofile" element={<MyProfile />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
 import logo from '../assets/logo.svg';
 
 const Navbar = () => (
-  <nav>
-    <div className="logoContainer">
-      <img src={logo} alt="logo" className="logo" />
-      <h1>Space Traveler&apos;s Hub</h1>
-    </div>
-    <ul className="linksContainer">
-      <li><a href="/">Rockets</a></li>
-      <li><a href="/">Missions</a></li>
-      {' '}
-      |
-      {' '}
-      <li><a href="/">My Profile</a></li>
-    </ul>
-  </nav>
+  <>
+    <nav>
+      <div className="logoContainer">
+        <img src={logo} alt="logo" className="logo" />
+        <h1>Space Traveler&apos;s Hub</h1>
+      </div>
+      <ul className="linksContainer">
+        <li><NavLink to="/">Rockets</NavLink></li>
+        <li><NavLink to="/missions">Missions</NavLink></li>
+        {' '}
+        |
+        {' '}
+        <li><NavLink to="/myprofile">My Profile</NavLink></li>
+      </ul>
+    </nav>
+    <Outlet />
+  </>
 );
 
 export default Navbar;
