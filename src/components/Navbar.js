19230d4ec8565d4f064bd5dbb91c8ab7634cbacb
Cristian Villa <c.villavirgen@gmail.com>
2023-09-05 13:55:53 -0600

    Add activeClassName to Rockets NavLink in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,12 +10,12 @@ const Navbar = () => (
         <h1>Space Traveler&apos;s Hub</h1>
       </div>
       <ul className="linksContainer">
-        <li><NavLink to="/">Rockets</NavLink></li>
-        <li><NavLink to="/missions">Missions</NavLink></li>
+        <li><NavLink to="/rockets" activeClassName="active">Rockets</NavLink></li>
+        <li><NavLink to="/missions" activeClassName="active">Missions</NavLink></li>
         {' '}
         |
         {' '}
-        <li><NavLink to="/myprofile">My Profile</NavLink></li>
+        <li><NavLink to="/myprofile" activeClassName="active">My Profile</NavLink></li>
       </ul>
     </nav>
     <Outlet />
