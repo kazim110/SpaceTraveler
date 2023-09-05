90e00d7f6d6cf4fe079afcbddcbb94e9cff92e49
Cristian Villa <c.villavirgen@gmail.com>
2023-09-05 16:54:46 -0600

    Implement Rockets component styling and data fetching

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from '../features/rockets/rocketsSlice';
+import './Rockets.css';
 
 const Rockets = () => {
   const rocketsList = useSelector((state) => state.rockets.rockets);
@@ -11,14 +12,16 @@ const Rockets = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>Rockets</h1>
+    <div className="rockets-container">
       {rocketsList.map((rocket) => (
-        <div key={rocket.id}>
-          <img src={rocket.flickr_images[0]} alt={rocket.name} />
-          <h2>{rocket.name}</h2>
-          <h5>{rocket.type}</h5>
-          <p>{rocket.description}</p>
+        <div className="rocket-card" key={rocket.id}>
+          <img className="rocket-image" src={rocket.flickr_images[0]} alt={rocket.name} />
+          <div className="rocket-info">
+            <h2>{rocket.name}</h2>
+            <p>{rocket.description}</p>
+            <h5>{rocket.rocket_type}</h5>
+            <button className="reserve-button" type="button">Reserve Rocket</button>
+          </div>
         </div>
       ))}
     </div>
