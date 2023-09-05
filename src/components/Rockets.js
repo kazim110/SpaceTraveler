da12b36396b0a53e1eba74c1b8e0b4d3ef2110d9
Cristian Villa <c.villavirgen@gmail.com>
2023-09-05 15:20:23 -0600

    Display fetched rockets and missions data in respective components

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../features/rockets/rocketsSlice';
 
-const Rockets = () => (
-  <div>Rockets</div>
-);
+const Rockets = () => {
+  const rocketsList = useSelector((state) => state.rockets.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>Rockets</h2>
+      {rocketsList.map((rocket) => (
+        <div key={rocket.id}>
+          <h2>{rocket.rocket_name}</h2>
+          <h3>{rocket.rocket_type}</h3>
+          <p>{rocket.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Rockets;
