04ea6ee2c3ebcb96a0554b24584cd316170640a6
kazim <kazim.mohammadi110@gmail.com>
2023-09-06 14:33:47 +0430

    Add join and leave state and its UI

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissions } from '../features/missions/missionsSlice';
+import { fetchMissions, joinMission, leaveMission } from '../features/missions/missionsSlice';
 
 const Missions = () => {
   const missionsList = useSelector((state) => state.missions.missions);
@@ -9,6 +9,14 @@ const Missions = () => {
   useEffect(() => {
     dispatch(fetchMissions());
   }, [dispatch]);
+
+  const handleJoinMission = (id) => {
+    dispatch(joinMission(id));
+  };
+
+  const handleLeaveMission = (id) => {
+    dispatch(leaveMission(id));
+  };
 
   return (
     <div>
@@ -26,8 +34,37 @@ const Missions = () => {
             <tr key={mission.id}>
               <td className="MissionName">{mission.mission_name}</td>
               <td className="MissionDescription">{mission.description}</td>
-              <td className="memberTd midlleAlign"><button type="button" className="btn btn-secondary text-nowrap membershipBtn">NOT A MEMBER</button></td>
-              <td className="joinBtnTd midlleAlign"><button type="button" className="btn text-nowrap btn-outline-dark joinMissioinBtn">Join Mission</button></td>
+              <td className="memberTd midlleAlign">
+                {mission.reserved ? (
+                  <span className="badge bg-info">Active Member</span>
+                ) : (
+                  <span className="badge bg-secondary">NOT A MEMBER</span>
+                )}
+              </td>
+              <td className="joinBtnTd midlleAlign">
+                {mission.reserved ? (
+                  <button
+                    type="button"
+                    className="btn text-nowrap btn-outline-danger leaveMissionBtn"
+                    onClick={() => {
+                      handleLeaveMission(mission.id);
+                    }}
+                  >
+                    Leave Mission
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="btn text-nowrap btn-outline-dark joinMissioinBtn"
+                    onClick={() => {
+                      handleJoinMission(mission.id);
+                    }}
+                  >
+                    Join Mission
+                  </button>
+                )}
+
+              </td>
             </tr>
           ))}
         </tbody>
