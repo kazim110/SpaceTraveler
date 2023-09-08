833f85d404efc5ff848b328702c8d5040c1efd8f
Cristian Villa <c.villavirgen@gmail.com>
2023-09-07 21:55:22 -0600

    Fix warnings and add missing dependencies for tests

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { render, waitFor } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Navbar from './Navbar';
 
 test('renders Navbar component', () => {
-  render(<Navbar />);
+  render(
+    <Router>
+      <Navbar />
+    </Router>,
+  );
 });
 
-test('active class changes on link click', () => {
-  const { getByText } = render(<Router><Navbar /></Router>);
+test('active class changes on link click', async () => {
+  const { getByText } = render(
+    <Router>
+      <Navbar />
+    </Router>,
+  );
   const link = getByText('Rockets');
   userEvent.click(link);
-  expect(link.classList.contains('active')).toBe(true);
+
+  await waitFor(() => {
+    expect(link.classList.contains('active')).toBe(true);
+  });
 });
